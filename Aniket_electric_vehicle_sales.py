f93ddcb38957c041e40a71f2fa2d715c1ec220d0
ANIKETGUP3838 <144110642+ANIKETGUP3838@users.noreply.github.com>
2025-05-19 21:00:05 +0530

    Update Aniket_electric_vehicle_sales.py

--- Aniket_electric_vehicle_sales.py
+++ Aniket_electric_vehicle_sales.py
@@ -8,6 +8,8 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
+import pydeck as pdk
+import plotly.express as px
 
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
